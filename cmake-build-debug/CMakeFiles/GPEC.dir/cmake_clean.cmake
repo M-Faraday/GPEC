1cd8eb181f1dbdb8f0319c73a77ddf70faa8abef
M-Faraday <besterrudolph@gmail.com>
2018-07-12 00:00:05 +0200

    Intial IDE Project Creation

---
+++ cmake-build-debug/CMakeFiles/GPEC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPEC.dir/main.cpp.obj"
+  "GPEC.pdb"
+  "GPEC.exe"
+  "GPEC.exe.manifest"
+  "GPEC.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPEC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
